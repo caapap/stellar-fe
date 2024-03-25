ed9fef31d8b889a57c07f9da8f0417f7460a5d87
caapap <caapap@qq.com>
2024-03-25 20:46:03 +0800

    replace logo with subtitle

--- src/components/pageLayout/index.tsx
+++ src/components/pageLayout/index.tsx
@@ -133,7 +133,7 @@ const PageLayout: React.FC<IPageLayoutProps> = ({ icon, title, rightArea, introI
                   <Version />
 
                   {/* 整合版本关闭文档链接 */}
-                  {import.meta.env.VITE_IS_ENT !== 'true' && (
+                  {/* {import.meta.env.VITE_IS_ENT !== 'true' && (
                     <Space style={{ marginRight: 16 }}>
                       <div style={{ marginRight: 32, position: 'relative' }}>
                         <a target='_blank' href={siteInfo?.document_url || 'https://flashcat.cloud/docs/'}>
@@ -159,7 +159,7 @@ const PageLayout: React.FC<IPageLayoutProps> = ({ icon, title, rightArea, introI
                         </AdvancedWrap>
                       )}
                     </Space>
-                  )}
+                  )}  */}
 
                   {rightArea}
 
